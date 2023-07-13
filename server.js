ef513386548ff9567fe3f990eba71cf34e7b9804
ericomurta1991 <erico_murta@yahoo.com.br>
2023-07-13 10:50:37 -0300

    adicionando as rotas em server.js e adicionando gitignore

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require("http");
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso de node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'Listagem de autores',
+    '/sobre' : 'info sobre o projeto'
+}
+
 const server = http.createServer((req, res) =>{
     res.writeHead(200, {'Content-Type' : 'text/plain'})
-    res.end('Curso de node');
+    res.end(rotas[req.url]);
 })
 
 server.listen(port, () => {
