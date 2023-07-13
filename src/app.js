092f99550cdfa25cb798f69893c62121b369480e
ericomurta1991 <erico_murta@yahoo.com.br>
2023-07-13 14:43:40 -0300

    Adicionado metodo get, get por id, put e delete.

--- src/app.js
+++ src/app.js
@@ -17,9 +17,33 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros)
 })
 
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+})
+
 app.post('/livros', (req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro foi cadastrado com sucesso');
 })
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+})
+
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    livros.splice(index, 1);
+    res.send(`Livro ${id} removido com sucesso`);
+})
+
+function buscaLivro(id){
+    return livros.findIndex(livro => livro.id == id);
+}
+
+
 
 export default app
