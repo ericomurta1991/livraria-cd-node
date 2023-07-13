eec39a7fe9e138b7bca5cfeb2cf07996911b51fc
ericomurta1991 <erico_murta@yahoo.com.br>
2023-07-13 10:04:07 -0300

    subindo server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer((req, res) =>{
+    res.writeHead(200, {'Content-Type' : 'text/plain'})
+    res.end('Curso de node');
+})
+
+server.listen(port, () => {
+    console.log(`Servidor escutando em http:://localhost:${port}`)
+})
