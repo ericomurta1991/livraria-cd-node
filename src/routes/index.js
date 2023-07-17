4ef713a9308367e7cb919440c9b151f111a53dea
ericomurta1991 <erico_murta@yahoo.com.br>
2023-07-17 15:33:33 -0300

    criado livrosRoutes.js com as rotas get, post e put

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import  express from "express";
+import livros from "../routes/livrosRoutes.js";
+
+const routes = (app) => {
+    app.route("/").get((req, res) =>{
+        res.status(200).send(({titulo: "curso de node"}))
+})
+
+
+app.use(
+    express.json(),
+    livros
+  )
+}
+export default routes;
