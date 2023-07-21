8085d6ba3c5d36e81b99d3c9b718ac4371ad1af3
ericomurta1991 <erico_murta@yahoo.com.br>
2023-07-21 15:30:29 -0300

    import do  dotenv

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import  express from "express";
 import db from "./config/dbConnect.js";
-import routes from "./routes/index.js"
+import routes from "./routes/index.js";
 
-db.on("error", console.log.bind(console, 'Erro de conexao'));
+db.on("error", console.log.bind(console, "Erro de conexao"));
 db.once("open", () => {
-    console.log('Conexao com o banco feita com sucesso')
-})
+  console.log("Conexao com o banco feita com sucesso");
+});
 
 const app = express();
 
@@ -13,4 +13,4 @@ app.use(express.json());
 
 routes(app);
 
-export default app
+export default app;
