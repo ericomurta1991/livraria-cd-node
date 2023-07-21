49ddd94f87bcc69e52d9f0a81a3954a8049c6568
ericomurta1991 <erico_murta@yahoo.com.br>
2023-07-21 15:24:15 -0300

    import corrigido

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,15 +1,15 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema(
-    {
-        id: {type: String},
-        titulo: {type: String, required: true},
-        autor: {type: mongoose.Schema.Types.ObjectId, ref: "autores", required: true},
-        editora: {type: String, required: true},
-        numeroDePaginas: {type: Number}
-    }
+  {
+    id: {type: String},
+    titulo: {type: String, required: true},
+    autor: {type: mongoose.Schema.Types.ObjectId, ref: "autores", required: true},
+    editora: {type: String, required: true},
+    numeroDePaginas: {type: Number}
+  }
 );
 
-const livros= mongoose.model('livros', livroSchema);
+const livros= mongoose.model("livros", livroSchema);
 
 export default livros;
