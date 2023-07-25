39a258aa26fa8ceb0c96611ee3e8adf7daf4a410
ericomurta1991 <erico_murta@yahoo.com.br>
2023-07-25 15:13:00 -0300

    adicionado mensagem para erro em caso de falta dos campos obrigatorios, no model Livro.js e em Autor.js

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,13 +3,20 @@ import mongoose from "mongoose";
 const livroSchema = new mongoose.Schema(
   {
     id: {type: String},
-    titulo: {type: String, required: true},
-    autor: {type: mongoose.Schema.Types.ObjectId, ref: "autores", required: true},
-    editora: {type: String, required: true},
+    titulo: {
+      type: String,
+      required: [true, "O titulo do livro `e obrigatorio"]
+    },
+    autor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "autores",
+      required: [true, "O autor(a) do(a) livro `e obrigatorio"]},
+    editora: {
+      type: String,
+      required: [true, "A editora `e obrigatoria"]},
     numeroDePaginas: {type: Number}
   }
 );
 
 const livros= mongoose.model("livros", livroSchema);
-
 export default livros;
