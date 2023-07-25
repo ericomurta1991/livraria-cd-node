e5150df2ff1e69f91f3258f6701dd6d23374429c
ericomurta1991 <erico_murta@yahoo.com.br>
2023-07-25 15:10:46 -0300

    Adicionado middlewares para tratamento de cast erros e erros de validacao

---
+++ src/middlewares/manipuladorDeErros.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+// eslint-disable-next-line no-unused-vars
+function manipuladorDeErros(erro, req, res, next) {
+  if(erro instanceof mongoose.Error.CastError){
+    res.status(400).send({message: "Um ou mais dados fornecidos estao incorretos"});
+  }else if(erro instanceof mongoose.Error.ValidationError){
+    const mensagensErro = Object.values(erro.errors)
+      .map(erro => erro.message)
+      .join("; ");
+
+    res.status(400).send({message: `Os seguintes erros foram encontrados: ${mensagensErro}`}); 
+  }else {
+    res.status(500).send({message: "Erro interno do servidor."});
+  }
+}
+
+export default manipuladorDeErros;
