d846342a764974b9e1a5acdfb35ad3f1421c5e96
ericomurta1991 <erico_murta@yahoo.com.br>
2023-07-25 15:19:10 -0300

    setado manipuladorDeErros em app.use

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import  express from "express";
 import db from "./config/dbConnect.js";
 import routes from "./routes/index.js";
+import manipuladorDeErros from "./middlewares/manipuladorDeErros.js";
 
 db.on("error", console.log.bind(console, "Erro de conexao"));
 db.once("open", () => {
@@ -8,9 +9,11 @@ db.once("open", () => {
 });
 
 const app = express();
-
 app.use(express.json());
-
 routes(app);
+
+
+// eslint-disable-next-line no-unused-vars
+app.use(manipuladorDeErros);
 
 export default app;
