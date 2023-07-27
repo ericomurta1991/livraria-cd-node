7e7dc7839232542fdc5afa2582d015923654beaa
ericomurta1991 <erico_murta@yahoo.com.br>
2023-07-27 09:59:26 -0300

    adicionado numero maximo e numero minimo de paginas para cadastro no banco de dados e apenas a opcao de cadastro para duas editoras.

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -5,16 +5,25 @@ const livroSchema = new mongoose.Schema(
     id: {type: String},
     titulo: {
       type: String,
-      required: [true, "O titulo do livro `e obrigatorio"]
+      required: [true, "O titulo do livro `e obrigatorio."]
     },
     autor: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "autores",
-      required: [true, "O autor(a) do(a) livro `e obrigatorio"]},
+      required: [true, "O autor(a) do(a) livro `e obrigatorio."]},
     editora: {
       type: String,
-      required: [true, "A editora `e obrigatoria"]},
-    numeroDePaginas: {type: Number}
+      required: [true, "A editora `e obrigatoria."],
+      enum: {
+        values: ["Casa do Codigo", "Alura"],
+        message: "A editora {VALUE} nao `e um valor permitido."
+      }
+    },
+    numeroDePaginas: {
+      type: Number,
+      min: [10, "O numero de paginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
+      max: [5000, "O numero de paginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"]
+    }
   }
 );
 
